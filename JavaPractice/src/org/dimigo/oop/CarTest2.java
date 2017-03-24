77c3f3a952ee5ad69ef21867c3a301bbb5baa4c5
김은상 <@DESKTOP-3O0QVS7>
2017-03-24 14:28:08 +0900

    실습과제 4-2 자동차 출력

---
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ CarTest
+ *
+ * 1. 개요 :
+ * 2. 작성일  : 2017. 3. 23.
+ * </pre>
+ *
+ * @author : 김은상
+ * @version : 1.0
+ */
+public class CarTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Car car = new Car();
+		
+		System.out.println("<<자동차 목록>>");
+		System.out.println(car.getCompany());
+		System.out.println(car.getModel());
+		System.out.println(car.getColor());
+		System.out.println(car.getMaxSpeed() + "km");
+		System.out.println(String.format("%,d", car.getPrice()) + "원");
+		
+		System.out.println();
+		
+		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		
+		System.out.println(car2.getCompany());
+		System.out.println(car2.getModel());
+		System.out.println(car2.getColor());
+		System.out.println(car2.getMaxSpeed() + "km");
+		System.out.println(String.format("%,d", car2.getPrice()) + "원");
+
+		System.out.println();
+		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+
+		System.out.println(car3.getCompany());
+		System.out.println(car3.getModel());
+		System.out.println(car3.getColor());
+		System.out.println(car3.getMaxSpeed() + "km");
+		System.out.println(String.format("%,d", car3.getPrice()) + "원");
+		
+
+	}
+
+}
