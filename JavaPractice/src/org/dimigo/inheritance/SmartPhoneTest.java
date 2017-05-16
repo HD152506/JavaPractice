65cb6d3fa1fcff0f52900cfdffc15d49dc9cdc6a
김은상 <@DESKTOP-3O0QVS7>
2017-05-16 12:35:28 +0900

    실습과제 11 스마트폰 경쟁

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ SmartPhoneTest
+ *
+ * 1. 개요 :
+ * 2. 작성일  : 2017. 5. 12.
+ * </pre>
+ *
+ * @author : 김은상
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+
+		SmartPhone[] sm = {
+				new IPhone("iPhone 7", "애플", 900000),
+				new Galaxy("갤럭시 S8", "삼성", 800000)
+		};
+		
+		for(SmartPhone v: sm) {
+		System.out.println(v);
+		v.turnOn();
+		v.pay();
+		if(v instanceof IPhone) {
+			((IPhone) v).useAirDrop();
+		}
+		else if(v instanceof Galaxy)
+		{
+			((Galaxy) v).useWirelessCharging();
+		}
+		v.turnOff();
+		System.out.println();
+		}
+	}
+}
